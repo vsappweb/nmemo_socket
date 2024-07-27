9a39c4d3ed3d4d87cbd00b9aa99c9ff6a500f67c
VS hp <magnipython@gmail.com>
2024-07-27 14:02:36 +0200

    fix style with Prettier

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 // const ORIGIN = process.env.REACT_APP_ORIGIN
 const io = require("socket.io")(8900, {
-    cors: {
-        origin: "*",
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
 let users = [];
 
 const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId);
-}
+  users = users.filter((user) => user.socketId !== socketId);
+};
 
 const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-        users.push({ userId, socketId });
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
 };
 
 const getUser = (userId) => {
-    return users.find((user) => user.userId === userId)
-}
+  return users.find((user) => user.userId === userId);
+};
 
 io.on("connection", (socket) => {
-    //when connect
-    console.log("a user connected");
+  //when connect
+  console.log("a user connected");
 
-    //take userId and socketId from user
-    socket.on("sendUser", userId => {
-        addUser(userId, socket.id);
-        io.emit("getUsers", users);
+  //take userId and socketId from user
+  socket.on("sendUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
     });
+  });
 
-    //send and get message
-    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-        const user = getUser(receiverId);
-        io.to(user.socketId).emit("getMessage", {
-            senderId, text,
-        });
-    });
-
-
-
-    //when disconect
-    socket.on("disconnect", () => {
-        console.log("a user disconnected!");
-        removeUser(socket.id);
-        io.emit("getUsers", users);
-    })
+  //when disconect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 });
